7801873392726de530256338b299362b6ed0a5cc
Alec Schleicher <schleicher@3cisd.com>
2017-06-23 17:49:59 -0400

    stuff

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+
+function addSession() {
+    session = $("<div>");
+    session.addClass("session");
+    session.text("whats up");
+    $("#sessionList").append(session);
+}
+
+addSession();
+addSession();
+
+$("#sessionChat").text("blahsdgfds");
--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-const electron = require('electron');
-const {app, BrowserWindow} = electron;
-
-console.log(__dirname);
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
 
 app.on('ready', () => {
     let win = new BrowserWindow({width:1296, height: 732, autoHideMenuBar: true});
     win.loadURL(`file://${__dirname}/index.html`);
+
+    
+    function addSession() {
+        session = $("<div>");
+        session.addClass("session");
+        session.text("whats up");
+        $("#sessionList").append(session);
+    }
+
+    addSession();
+    addSession();
+
+    $("#sessionChat").text("blahsdgfds");
 
 });
