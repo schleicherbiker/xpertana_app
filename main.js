5afeee56b971c16241141b1a315a4f01a36c6ed7
Alec Schleicher <alecs@live.unc.edu>
2017-06-23 15:56:52 -0400

    File structure

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const electron = require('electron');
-const bootstrap = require('bootstrap@3');
 const {app, BrowserWindow} = electron;
 
 console.log(__dirname);
 
 app.on('ready', () => {
-    let win = new BrowserWindow({width:1080, height: 600});
+    let win = new BrowserWindow({width:1296, height: 732, autoHideMenuBar: true});
     win.loadURL(`file://${__dirname}/index.html`);
+
 });
